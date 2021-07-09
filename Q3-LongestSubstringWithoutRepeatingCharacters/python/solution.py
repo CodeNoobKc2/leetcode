062feaec4068ff584b34ddb0d59fa262b33a090b
CodeNoobKc2 <whitegivekingkc2@163.com>
2021-07-09 15:23:29 +0800

    Q3 68ms80.12% 14.9MB 89.81%

--- Q3-LongestSubstringWithoutRepeatingCharacters/python/solution.py
+++ Q3-LongestSubstringWithoutRepeatingCharacters/python/solution.py
@@ -1,40 +1,29 @@
 from typing import List,Set
 
-class Feature:
-    cur:str
-    longest:str
-
-    def __init__(self,cur:str):
-        self.cur = cur
-        self.longest:str = cur
-
-    def maxLen(self) -> int:
-        return len(self.longest)
-
-    def join(self,other):
-        for i in range(other.maxLen()):
-            if not self.cur in other.longest[:i+1]:
-                self.longest = self.longest + other.longest[i:i+1]
-                print(self.cur,self.longest,other.longest[:i+1])
-            else:
-                break
-
+# slide window
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         if len(s) == 0:
             return 0
 
-        features = [ Feature(x) for x in s]
         maxLen = 1
-        lastFeature = features[0]
-        for feature in features[1:]:
-            feature.join(lastFeature)
-            lastFeature = feature
-            maxLen = feature.maxLen() if feature.maxLen() > maxLen else maxLen
+        chars = set()
+        chars.add(s[0])
+        l = 0
+        r = 0
+        while l < len(s):
+            while r+1 < len(s) and s[r+1] not in chars:
+                r = r + 1
+                chars.add(s[r])
+
+            maxLen = len(chars) if len(chars) > maxLen else maxLen
+            chars.remove(s[l])
+            l = l+1
 
         return maxLen
 
 if __name__ == "__main__":
+    print(Solution().lengthOfLongestSubstring("aab"))
     print(Solution().lengthOfLongestSubstring("abcabcbb"))
     print(Solution().lengthOfLongestSubstring("aaaaaaaa"))
     print(Solution().lengthOfLongestSubstring("pwwkew"))
